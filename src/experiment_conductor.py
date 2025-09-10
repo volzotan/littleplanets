d76dd9238566eb707b48d689e44119e084c15101
Christopher Getschmann <christopher@volzo.de>
2025-09-10 21:00:52 +0200

    experiment_conductor: concurrency added

--- src/experiment_conductor.py
+++ src/experiment_conductor.py
@@ -1,19 +1,25 @@
 import math
 import os
+import shutil
 import subprocess
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
 from typing import Any
 
 import toml
+from loguru import logger
+
+NUM_WORKERS = 4
 
 CONFIG_OVERRIDE_FILE = Path("config_hatch_override.toml")
 
 PY_FILE = "src/hatch.py"
 INKSCAPE_BIN = "/Applications/Inkscape.app/Contents/MacOS/inkscape"
+TEMP_DIR = Path("experiment_temp")
 OUTPUT_DIR = Path("experiment_output")
 
 ARGUMENTS = [
-    "build/mapping_color.png",
+    "build/mapping_color.npy",
     "build/mapping_angle_5.png",
     "build/mapping_distance.png",
     "build/mapping_line_length.png",
@@ -21,63 +27,104 @@ ARGUMENTS = [
 ]
 
 VARIABLES = {
-    "blur_angle_kernel_size_perc": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.5, 2.0],
-    "flowlines_line_distance_end_factor": [0.0, 0.25, 0.5, 0.75, 1.0],
+    # "blur_angle_kernel_size_perc": [0.1, 0.4, 0.8, 1.2, 2.4],
+    # "blur_distance_kernel_size_perc": [0.1, 0.4, 0.8, 1.2, 2.4],
+    # "flowlines_line_distance_end_factor": [0.1, 0.25, 0.5, 0.75, 1.0],
+    # "flowlines_line_distance": [(0.6, 5), (0.6, 10), (0.6, 15), (0.6, 20)],
+    "flowlines_line_max_length": [(3, 10), (3, 20), (3, 30), (3, 40), (3, 50)],
+    "flowlines_max_angle_discontinuity": [math.pi / 12, math.pi / 6, math.pi / 3, math.pi / 1]
 }
 
-num_experiment = 0
-
-
-def make(config_overwrite) -> None:
-    global num_experiment
-
-    num_experiment += 1
-    print(f"Executing experiment {num_experiment:> 5}/{total_experiments}")
+def process(num_experiment: int, config_override: dict[str, Any]) -> None:
+    # logger.info(f"Executing experiment {num_experiment:> 5}")
 
     filename = f"experiment_{num_experiment}"
     config_file = OUTPUT_DIR / (filename + ".toml")
     image_file = OUTPUT_DIR / (filename + ".png")
 
     with open(config_file, "w") as f:
-        toml.dump(config_overwrite, f)
+        toml.dump(config_override, f)
 
-    subprocess.run(
-        [
-            "uv",
-            "run",
-            PY_FILE,
-            *ARGUMENTS,
-            "--config",
-            config_file,
-            "--contours",
-            "build/contours.npz",
-            "--output",
-            "build/littleplanets.svg",
-        ],
-        check=True,
-    )
+    svg_path = TEMP_DIR / f"{num_experiment}.svg"
 
-    subprocess.run(
-        [INKSCAPE_BIN, "build/littleplanets.svg", f"--export-filename={image_file}", "--export-width=2000", "--export-background=#000000"], check=True
-    )
+    try:
+        subprocess.run(
+            [
+                "uv",
+                "run",
+                PY_FILE,
+                *ARGUMENTS,
+                "--config",
+                config_file,
+                # "--contours",
+                # "build/contours.npz",
+                "--output",
+                svg_path,
+                "--palette-color", "240", "126", "50",
+                "--palette-color", "65", "102", "174",
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+
+        subprocess.run(
+            [
+                INKSCAPE_BIN,
+                svg_path,
+                f"--export-filename={image_file}",
+                "--export-width=2000",
+                "--export-background=#000000"
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True
+        )
+
+        os.remove(svg_path)
+
+    except Exception as e:
+        logger.error(f"Subprocess failed: {e}")
+        raise e
 
 
-def rec_looping(vars: dict[str, list[Any]], config_overwrite: dict[str, Any] = {}) -> None:
-    if len(vars.keys()) > 0:
-        key = list(vars.keys())[0]
-        for value in vars[key]:
-            config_overwrite[key] = value
-            vars_copy = vars.copy()
-            vars_copy.pop(key)
-            rec_looping(vars_copy, config_overwrite)
+def rec_looping(variables: dict[str, list[Any]], config_override: dict[str, Any] = {}) -> list[dict[str, Any]]:
+    if len(variables.keys()) > 0:
+        configs = []
+        key = list(variables.keys())[0]
+        for value in variables[key]:
+            config_override_copy = config_override.copy()
+            config_override_copy[key] = value
+
+            variables_copy = variables.copy()
+            variables_copy.pop(key)
+
+            configs += rec_looping(variables_copy, config_override_copy)
+        return configs
     else:
-        make(config_overwrite)
+        return [config_override]
 
 
-if __name__ == "__main__":
-    global total_experiments
+def main() -> None:
 
+    shutil.rmtree(TEMP_DIR)
+    shutil.rmtree(OUTPUT_DIR)
+    os.makedirs(TEMP_DIR, exist_ok=True)
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     total_experiments = math.prod([len(values) for values in VARIABLES.values()])
-    rec_looping(VARIABLES)
+    overrides = rec_looping(VARIABLES)
+
+    # [process(i, config_override) for i, config_override in enumerate(overrides)]
+
+    completed_experiments = 0
+    with ProcessPoolExecutor(NUM_WORKERS) as executor:
+        futures = [executor.submit(process, i, config_override) for i, config_override in enumerate(overrides)]
+
+        for future in as_completed(futures):
+            completed_experiments += 1
+            logger.info(f"processed {completed_experiments}/{total_experiments} | {(completed_experiments/total_experiments):5.2%}")
+
+if __name__ == "__main__":
+    main()
+
