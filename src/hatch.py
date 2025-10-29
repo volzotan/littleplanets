bugfix: hatch.py distance normalization and cleanup

src/hatch.py
@@ -179,11 +179,6 @@ if __name__ == "__main__":
     mapping_angle = _blur_raster(mapping_angle, config.blur_angle_kernel_size_perc)
     mapping_distance = _blur_raster(mapping_distance, config.blur_distance_kernel_size_perc)
 
-    if args.debug:
-        cv2.imwrite(str(DIR_DEBUG / f"hatch_mapping_color{args.suffix}.png"), mapping_color)
-        cv2.imwrite(str(DIR_DEBUG / f"hatch_mapping_angle{args.suffix}.png"), mapping_angle)
-        cv2.imwrite(str(DIR_DEBUG / f"hatch_mapping_distance{args.suffix}.png"), mapping_distance)
-
     scaling_factor = config.dimensions[0] / mapping_angle.shape[1]
 
     linestrings_cutouts = []
@@ -215,7 +210,9 @@ if __name__ == "__main__":
     for ls in linestrings_cutouts:
         exclusion_points += shapely.get_coordinates(ls.segmentize(0.01)).tolist()
 
-    mapping_distance = ((mapping_distance - np.min(mapping_distance)) / np.ptp(mapping_distance) * 255).astype(np.uint8)
+    mask = mapping_background == 0
+    mapping_distance = ((mapping_distance - np.min(mapping_distance[mask])) / np.ptp(mapping_distance[mask]) * 255).astype(np.uint8)
+    mapping_distance[~mask] = 0
 
     # all areas above/below a brightness threshold should be kept empty
     # if not INVERT_COLOR:
@@ -226,6 +223,11 @@ if __name__ == "__main__":
     if config.invert_color:
         # white ink on black paper, invert grayscale image
         mapping_distance = ~mapping_distance
+
+    if args.debug:
+        # cv2.imwrite(str(DIR_DEBUG / f"hatch_mapping_color{args.suffix}.png"), mapping_color) # doesn't make sense to export mapping_color if it's palette colors
+        cv2.imwrite(str(DIR_DEBUG / f"hatch_mapping_angle{args.suffix}.png"), mapping_angle)
+        cv2.imwrite(str(DIR_DEBUG / f"hatch_mapping_distance{args.suffix}.png"), mapping_distance)
 
     # mapping_distance = np.zeros_like(mapping_angle, dtype=np.uint8)
     # mapping_line_length = np.zeros_like(mapping_angle)

src/palette.py
@@ -12,6 +12,9 @@ DIR_DEBUG = Path("debug")
 
 MIN_RATIO_THRESHOLD = 0.15
 
+CONTRAST_ENHANCEMENT = False
+CONTRAST_VALUE = 1.0
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("image", type=Path, default="image.tif", help="RGB image (TIFF)")
@@ -41,10 +44,10 @@ if __name__ == "__main__":
 
     if args.debug:  # TODO: remove hardcoded paths
         mapping_distance = cv2.imread("build/mapping_color.png", cv2.IMREAD_GRAYSCALE)
-        mapping_flat = cv2.imread("build/mapping_flat.png", cv2.IMREAD_GRAYSCALE)
+        mapping_background = cv2.imread("build/mapping_background.png", cv2.IMREAD_GRAYSCALE)
         if RESIZE_SIZE is not None:
             mapping_distance = cv2.resize(mapping_distance, RESIZE_SIZE)
-            mapping_flat = cv2.resize(mapping_flat, RESIZE_SIZE)
+            mapping_background = cv2.resize(mapping_background, RESIZE_SIZE)
 
         cv2.imwrite(str(DIR_DEBUG / "palette_mapping_color.png"), mapping_color)
         cv2.imwrite(str(DIR_DEBUG / "palette_mapping_distance.png"), mapping_distance)
@@ -136,7 +139,11 @@ if __name__ == "__main__":
     # i.e. it is the "too bright" error value.
     # The error of "too dark" is not relevant, since making a pixel brighter than the mixed color
     # is not possible (assuming bright colors on dark surface)
-    diff_v = np.clip(mapping_palette_avg_hsv[:, :, 2].astype(np.float32) - mapping_color_hsv[:, :, 2], 0, 255).astype(np.uint8)
+    diff_v = np.clip(mapping_palette_avg_hsv[:, :, 2].astype(float) - mapping_color_hsv[:, :, 2], 0, 255).astype(np.uint8)
+
+    if CONTRAST_ENHANCEMENT:
+        clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8, 8))
+        diff_v = cv2.addWeighted(clahe.apply(diff_v), CONTRAST_VALUE, diff_v, 1 - CONTRAST_VALUE, 0.0)
 
     # mapping_palette_avg_hsv[:, :, 2] -= diff_v
 
@@ -161,7 +168,7 @@ if __name__ == "__main__":
     mapping_palette_closest = palette[np.argmin(color_distance_error, axis=2)]
 
     if args.debug:
-        mapping_palette_closest[mapping_flat > 0] = [0, 0, 0]
+        mapping_palette_closest[mapping_background > 0] = [0, 0, 0]
         cv2.imwrite(str(DIR_DEBUG / "palette_mapping_hsv_closest.png"), cv2.cvtColor(mapping_palette_closest, cv2.COLOR_RGB2BGR))
 
     # LAB AVG
@@ -186,7 +193,7 @@ if __name__ == "__main__":
     weighted_colors = weighted_colors * ratio[:, :, :, np.newaxis]
     mapping_palette_avg_rgb = np.sum(weighted_colors, axis=2).astype(np.uint8)
 
-    # mapping_palette_avg_rgb[mapping_flat > 0] = [0, 0, 0]
+    # mapping_palette_avg_rgb[mapping_background > 0] = [0, 0, 0]
 
     mapping_palette_avg_hsv = cv2.cvtColor(mapping_palette_avg_rgb, cv2.COLOR_RGB2HSV)
     diff_v = np.clip(mapping_palette_avg_hsv[:, :, 2].astype(np.float32) - mapping_color_hsv[:, :, 2], 0, 255).astype(np.uint8)
@@ -208,7 +215,7 @@ if __name__ == "__main__":
     mapping_palette_closest = palette[np.argmin(color_distance_error, axis=2)]
 
     if args.debug:
-        mapping_palette_closest[mapping_flat > 0] = [0, 0, 0]
+        mapping_palette_closest[mapping_background > 0] = [0, 0, 0]
         cv2.imwrite(str(DIR_DEBUG / "palette_mapping_lab_closest.png"), cv2.cvtColor(mapping_palette_closest, cv2.COLOR_RGB2BGR))
 
     # HSV LAB AVG

src/process_blender.py
@@ -145,16 +145,6 @@ def _export_angles(arr: np.ndarray, adjust_y_axis: bool = False) -> np.ndarray:
     return mapping_angle
 
 
-def _adjust_contrast_brightness(img, contrast: float = 1.0, brightness: int = 0) -> np.ndarray:
-    """
-    Adjusts contrast and brightness of an uint8 image.
-    contrast:   (0.0,  inf) with 1.0 leaving the contrast as is
-    brightness: [-255, 255] with 0 leaving the brightness as is
-    """
-    brightness += int(round(255 * (1 - contrast) / 2))
-    return cv2.addWeighted(img, contrast, img, 0, brightness)
-
-
 def _apply_clipping(m: np.ndarray, start_percentile: float, end_percentile: float) -> np.ndarray:
     m_no_nan = np.nan_to_num(m)
     minval = np.percentile(m_no_nan, start_percentile)
@@ -200,7 +190,7 @@ def _apply_linear_slope(m: np.ndarray, slope_start: float, slope_end: float, cli
 
 
 def _apply_linear_transition(m: np.ndarray, min: float, max: float) -> np.ndarray:
-    return (np.clip(m, min, max) - min) / (max-min)
+    return (np.clip(m, min, max) - min) / (max - min)
 
 
 if __name__ == "__main__":
@@ -399,7 +389,7 @@ if __name__ == "__main__":
     # image space - blend:
 
     mixture = _apply_linear_transition(img_elevation_magnitude, 0.035, 0.06)
-    img_field_elevation_vectors_10 = img_direction_is * (1 - mixture)[:, :, np.newaxis] + img_elevation_direction_is * mixture[: ,:, np.newaxis]
+    img_field_elevation_vectors_10 = img_direction_is * (1 - mixture)[:, :, np.newaxis] + img_elevation_direction_is * mixture[:, :, np.newaxis]
 
     # ---
 
@@ -506,13 +496,6 @@ if __name__ == "__main__":
 
     if EXPORT:
         if CONTRAST_ENHANCEMENT:
-            # evaluating a good CONTRAST_VALUE:
-            # for i in range(1, 10):
-            #     contrast = 1.0+i/10
-            #     cv2.imwrite(str(args.output / f"mapping_distance_{contrast:5.2f}.png"), adjust_contrast_brightness(mapping_distance, contrast=contrast))
-
-            # mapping_distance = adjust_contrast_brightness(mapping_distance, contrast=CONTRAST_VALUE)
-
             clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8, 8))
             mapping_distance = cv2.addWeighted(clahe.apply(mapping_distance), CONTRAST_VALUE, mapping_distance, 1 - CONTRAST_VALUE, 0.0)
 