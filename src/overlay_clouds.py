overlay_clouds: bugfix rotation during image space conversion

src/overlay_clouds.py
@@ -8,10 +8,12 @@ from pydantic import BaseModel
 import numpy as np
 import math
 import pyvista as pv
+import matplotlib.pyplot as plt
 
 from process_blender import project_vectors_to_image_space
 from util.misc import rotate_points_inv, project_to_image_space, export_angles
 
+VISUALIZE = False
 DEBUG = True
 DIR_DEBUG = Path("debug")
 
@@ -25,6 +27,27 @@ class OverlayCloudsConfig(BaseModel):
     morph_kernel_size: int | None = 5
 
     threshold: int = 150
+
+
+def _visualize(u: np.ndarray, v: np.ndarray, path_base_image: Path = Path("output.png")) -> None:
+    size = (150, 75)
+
+    u = cv2.resize(u, size)
+    v = cv2.resize(v, size)
+
+    x = np.arange(u.shape[1])
+    y = np.arange(u.shape[0])
+    X, Y = np.meshgrid(x, y)
+
+    plt.figure(figsize=(30, 15))
+
+    if path_base_image.exists():
+        base_image = cv2.imread(str(path_base_image))
+        base_image = cv2.resize(base_image, (int(size[0] * 10), int(size[1] * 10))).astype(float) / 255
+        plt.imshow(base_image, cmap="gray", origin="upper")
+
+    plt.quiver(X * 10, Y * 10, u, v, color="red", headwidth=1)
+    plt.show()
 
 
 def _map(raster: np.ndarray, lat: float, lon: float) -> np.ndarray:
@@ -45,10 +68,18 @@ def _rotate_unit_vectors(vectors: np.ndarray, angles_2d: np.ndarray, step_distan
 
     v = vectors.reshape([-1, 3])
 
+    ys = step_distance * np.cos(angles_2d)
     xs = step_distance * np.sin(angles_2d)
-    zs = step_distance * np.cos(angles_2d)
+
+    ys = ys.reshape([-1])
     xs = xs.reshape([-1])
-    zs = zs.reshape([-1])
+
+    Ry = np.zeros((len(ys), 3, 3))
+    Ry[:, 0, 0] = np.cos(ys)
+    Ry[:, 0, 2] = np.sin(ys)
+    Ry[:, 1, 1] = 1
+    Ry[:, 2, 0] = -np.sin(ys)
+    Ry[:, 2, 2] = np.cos(ys)
 
     Rx = np.zeros((len(xs), 3, 3))
     Rx[:, 0, 0] = 1
@@ -57,70 +88,10 @@ def _rotate_unit_vectors(vectors: np.ndarray, angles_2d: np.ndarray, step_distan
     Rx[:, 2, 1] = np.sin(xs)
     Rx[:, 2, 2] = np.cos(xs)
 
-    Rz = np.zeros((len(zs), 3, 3))
-    Rz[:, 0, 0] = np.cos(zs)
-    Rz[:, 0, 1] = -np.sin(zs)
-    Rz[:, 1, 0] = np.sin(zs)
-    Rz[:, 1, 1] = np.cos(zs)
-    Rz[:, 2, 2] = 1
-
-    R = Rz @ Rx
+    R = Ry @ Rx
 
     rotated_v = np.einsum("nij,nj->ni", R, v)
     return rotated_v.reshape(vectors.shape)
-
-
-def _visualize(centers: np.ndarray, vectors: list[np.ndarray], points: list[np.ndarray], light_axis: np.ndarray = None) -> pv.Plotter:
-    plotter = pv.Plotter()
-
-    plotter.camera.position = (0.0, 0.0, 5.0)
-    plotter.camera.focal_point = (0.0, 0.0, 0.0)
-    plotter.camera.up = (0, 1, 0)
-
-    # X, Y, Z axes
-    plotter.add_mesh(
-        pv.Spline(np.array([[0, 0, 0], [1, 0, 0]]), 10).tube(radius=0.005),
-        color=[255, 0, 0],
-    )
-    plotter.add_mesh(
-        pv.Spline(np.array([[0, 0, 0], [0, 1, 0]]), 10).tube(radius=0.005),
-        color=[0, 255, 0],
-    )
-    plotter.add_mesh(
-        pv.Spline(np.array([[0, 0, 0], [0, 0, 1]]), 10).tube(radius=0.005),
-        color=[0, 0, 255],
-    )
-
-    # light axis
-    if light_axis is not None:
-        spline = pv.Spline(np.array([[0, 0, 0], light_axis], dtype=np.float32)).tube(radius=0.005)
-        plotter.add_mesh(spline, color=[255, 255, 0])
-
-    colors = ["red", "green", "blue"]
-
-    for vi, vector in enumerate(vectors):
-        for i in range(len(vector)):
-            if np.isnan(np.sum(centers[i])):
-                continue
-
-            if np.isnan(np.sum(vector[i])):
-                continue
-
-            if np.sum(np.abs(vector[i])) == 0.0:
-                continue
-
-            arrow = pv.Arrow(centers[i], vector[i], scale=0.05)
-            plotter.add_mesh(arrow, color=colors[vi])
-
-            # line = pv.Line(centers[i], centers[i] + vector[i])
-            # plotter.add_mesh(line)
-
-    for pi, point_set in enumerate(points):
-        for point in point_set:
-            sphere = pv.Sphere(0.005, point)
-            plotter.add_mesh(sphere, color=colors[pi])
-
-    return plotter
 
 
 def main() -> None:
@@ -148,28 +119,7 @@ def main() -> None:
     img_pxpos_back = np.load(args.raytrace_backface)
     projection_matrix = np.load(args.projection_matrix)
 
-    # TEST
-
-    # vectors = np.zeros([1, 3, 3])
-    # vectors[0, 0, :] = [1, 1, 0]
-    # vectors[0, 1, :] = [1, 1, 0]
-    # vectors[0, 2, :] = [1, 1, 0]
-    #
-    # angles = np.zeros([1, 3])
-    # angles[0, 0] = math.radians(0) / math.pi
-    # angles[0, 1] = math.radians(45) / math.pi
-    # angles[0, 2] = math.radians(90) / math.pi
-    #
-    # result = _rotate_unit_vectors(
-    #     vectors, angles, 0.3
-    # )
-    #
-    # print(result)
-    #
-    # _visualize([], [], [result.reshape([-1, 3])]).show()
-    # exit()
-
-    # WRITE CLOUD COVERAGE / WIND DIRECTION RASTER
+    # READ CLOUD COVERAGE / WIND DIRECTION RASTER
 
     with netCDF4.Dataset(args.netcdf, "r", format="NETCDF4") as data:
         # convert xarray to numpy ndarray, fill empty pixels with zeros
@@ -184,20 +134,23 @@ def main() -> None:
 
         u = data.variables["u10"][:].filled(0)[0, :, :]
         v = data.variables["v10"][:].filled(0)[0, :, :]
-        mapping_angle = np.atan2(u, v)
+        mapping_angle = np.atan2(v, u)
+
+        if VISUALIZE:
+            _visualize(u, v)
 
     if config.blur_kernel_size is not None and config.blur_kernel_size >= 3:
         mapping_clouds = cv2.blur(mapping_clouds, (config.blur_kernel_size, config.blur_kernel_size))
-
-    # shift center by 1/4 (adjust for X/Y axis change)
-    mapping_clouds = np.roll(mapping_clouds, -int(mapping_clouds.shape[1] / 4), axis=1)
-    mapping_angle = np.roll(mapping_angle, -int(mapping_angle.shape[1] / 4), axis=1)
-    lsm = np.roll(lsm, -int(lsm.shape[1] / 4), axis=1)
 
     if DEBUG:
         cv2.imwrite(str(DIR_DEBUG / "land_sea_mask.png"), (lsm * 255).astype(np.uint8))
         cv2.imwrite(str(DIR_DEBUG / "clouds_mapping_clouds.png"), mapping_clouds)
         cv2.imwrite(str(DIR_DEBUG / "clouds_mapping_angle.png"), (mapping_angle / math.tau * 255).astype(np.uint8))
+
+    # shift center by 1/4 (adjust for X/Y axis change)
+    mapping_clouds = np.roll(mapping_clouds, -int(mapping_clouds.shape[1] / 4), axis=1)
+    mapping_angle = np.roll(mapping_angle, -int(mapping_angle.shape[1] / 4), axis=1)
+    lsm = np.roll(lsm, -int(lsm.shape[1] / 4), axis=1)
 
     # ROTATE
 
@@ -211,7 +164,6 @@ def main() -> None:
 
     for y in range(img_pxpos_front.shape[0]):
         for x in range(img_pxpos_front.shape[1]):
-
             pf = img_pxpos_front[y, x]
             if not np.isnan(np.sum(pf)):
                 pf = rotate_points_inv([pf], *(blender_rotation))[0]
@@ -223,7 +175,6 @@ def main() -> None:
                 clouds_rotated_front[y, x] = _map(mapping_clouds, lat, lon)
                 angles_rotated_front[y, x] = _map(mapping_angle, lat, lon)
                 lsm_rotated_front[y, x] = _map(lsm, lat, lon)
-
 
             pb = img_pxpos_back[y, x]
             if not np.isnan(np.sum(pb)):
@@ -262,8 +213,6 @@ def main() -> None:
     # BACKGROUND
 
     def create_background(raycast: np.ndarray, clouds_mapping: np.ndarray, config: OverlayCloudsConfig) -> np.ndarray:
-
-
         mapping_background = np.zeros(raycast.shape[0:2], dtype=np.uint8)
         mapping_background[np.isnan(np.sum(raycast, axis=2))] = 255
 
@@ -289,24 +238,10 @@ def main() -> None:
     mapping_background_front = create_background(img_pxpos_front, clouds_rotated_front, config)
     mapping_background_back = create_background(img_pxpos_back, clouds_rotated_back, config)
 
-    # VISUALIZE
-
-    # if args.visualize:
-    #     visualize_linestrings([LineString([[0, 0, 0], p]) for p in ps]).show()
-
     # EXPORT
-
-    # bgmask = np.isnan(np.sum(img_pxpos, axis=2))
-    # image_rotated = np.zeros_like(image_rotated)
-    # image_rotated[~bgmask] = 230
-    # image_rotated[500, 500] = 0
-    # image_rotated[501, 500] = 255
-    # mapping_background = np.zeros_like(mapping_background)
-    # mapping_background[bgmask] = 255
 
     def _clip_and_rescale(raster: np.ndarray, min_value: float | int) -> np.ndarray:
         return (np.clip(raster, min_value, 255) - min_value) / (255 - min_value) * 255
-
 
     cv2.imwrite(str(args.output / "clouds_mapping_front_angle.png"), export_angles(direction_is_front, adjust_y_axis=True))
     cv2.imwrite(str(args.output / "clouds_mapping_front_distance.png"), _clip_and_rescale(clouds_rotated_front, config.threshold))
@@ -315,7 +250,6 @@ def main() -> None:
     cv2.imwrite(str(args.output / "clouds_mapping_back_angle.png"), export_angles(direction_is_back, adjust_y_axis=True))
     cv2.imwrite(str(args.output / "clouds_mapping_back_distance.png"), _clip_and_rescale(clouds_rotated_back, config.threshold))
     cv2.imwrite(str(args.output / "clouds_mapping_back_background.png"), mapping_background_back)
-
 
 
 if __name__ == "__main__":